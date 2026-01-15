0df6a188e7bd6d712f64eb632cc78c9758d45b57
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-15 20:05:54 +0530

    pascal_triangle

---
+++ 06_functions/04_pascal_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int factorial (int x) {
+    int fact = 1;
+    for (int i=2;i<=x;i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+int combination (int n,int r) {
+    int nCr = factorial(n) / (factorial(r) * factorial(n-r));
+    return nCr;
+}
+int main() {
+    int n;
+    printf("enter number of rows: ");
+    scanf("%d",&n);
+    for (int i=0;i<=n;i++) {
+        for (int j=0;j<=i;j++) {
+            int iCj = combination(i,j);
+            printf("%d ",iCj);
+        }
+        printf("\n");
+    }
+}
